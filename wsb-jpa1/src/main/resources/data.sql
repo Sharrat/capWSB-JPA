e58dff1027149ff245db4c8b8a15a71fb841373b
Adam Sołtysiak <adam.soltysiak.dev@gmail.com>
2024-11-23 17:02:59 +0100

    Added more example initial data to database

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -1,2 +1,10 @@
 insert into address (id, address_line1, address_line2, city, postal_code)
             values (1, 'xx', 'yy', 'city', '62-030');
+insert into doctor (id, doctor_number, email, first_name, last_name, specialization, telephone_number)
+            values (1, '123', 'Adam@gmail.com', 'Adam', 'Nych', 'Dentist', '123456789');
+insert into medical_treatment (id, description, type)
+            values (1, 'xx', 'yy');
+insert into patient (id, date_of_birth, email, first_name, last_name, patient_number, telephone_number)
+            values (1, to_date('1996-12-12','yyyy-mm-dd'), 'Badam@gmail.com', 'Milosz', 'Mikulski', '123', '123456789');
+insert into visit (id, description, time)
+            values (1, 'xx',  '2024-06-24 00:00:00');
