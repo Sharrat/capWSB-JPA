dec2b2e6d79c9706f093fb70d6290897a871ae6f
Adam Sołtysiak <adam.soltysiak.dev@gmail.com>
2025-01-11 13:14:42 +0100

    Code correction from lab2

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/VisitDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/VisitDaoImpl.java
@@ -3,18 +3,13 @@ package com.jpacourse.persistence.dao.impl;
 import com.jpacourse.persistence.dao.VisitDao;
 import com.jpacourse.persistence.entity.VisitEntity;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class VisitDaoImpl extends AbstractDao<VisitEntity, Long> implements VisitDao {
-    @PersistenceContext
-    private EntityManager em;
 
     @Override
     public List<VisitEntity> findByPatientId(Long patientId) {
-        return em.createQuery("SELECT a FROM VisitEntity a WHERE a.patient.id = :patientId", VisitEntity.class).setParameter("patientId", patientId).getResultList();
+        return entityManager.createQuery("SELECT a FROM VisitEntity a WHERE a.patient.id = :patientId", VisitEntity.class).setParameter("patientId", patientId).getResultList();
     }
 }
